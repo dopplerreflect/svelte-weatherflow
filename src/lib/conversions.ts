2e608b91d69e22fe48adeeefee2a145dfe52f1e8
David Rose <doppler@gmail.com>
2022-12-23 07:26:12 -0600

    add celsiusToFarenheit and round to one decimal place

--- src/lib/conversions.ts
+++ src/lib/conversions.ts
@@ -1,3 +1,7 @@
 export function mpsToMph(value: number): number {
-	return value * 2.23694;
+	return Number((value * 2.23694).toFixed(1));
+}
+
+export function celsiusToFarenheit(value: number): number {
+	return Number(((value * 9) / 5 + 32).toFixed(1));
 }
