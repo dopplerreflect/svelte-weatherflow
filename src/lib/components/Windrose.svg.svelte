e5ed6885bf50777fa64d138af088e63a8d2333b1
David Rose <doppler@gmail.com>
2022-12-26 10:37:14 -0600

    make it glow!

--- src/lib/components/Windrose.svg.svelte
+++ src/lib/components/Windrose.svg.svelte
@@ -24,7 +24,7 @@
 	$: maxSpeed = Math.max(...$rapid_wind.map((e) => mpsToMph(e.speed)));
 	$: ringRadii = Array.from({ length: Math.ceil(maxSpeed) })
 		.map((_, i) => {
-			const div = maxSpeed > 5.9 ? 5 : 1;
+			const div = maxSpeed > 9.9 ? 5 : 1;
 			if (i % div === 0) {
 				return i;
 			}
@@ -40,8 +40,12 @@
 			<path d="M-100,-100H100V100H-100Z" fill="white" />
 			<path d="M0,-6H100V6H0Z" fill="black" />
 		</mask>
+		<filter id="blur">
+			<feGaussianBlur in="SourceGraphic" stdDeviation="0.75" />
+		</filter>
 	</defs>
-	<circle cx={0} cy={0} r={100} fill="hsl(240, 100%, 10%)" stroke="white" />
+	<circle cx={0} cy={0} r={100} fill="hsl(240, 100%, 10%)" stroke="white" filter="url(#blur)" />
+	<circle cx={0} cy={0} r={100} fill="none" stroke="white" stroke-width="0.25" />
 	<g id="ringRadii">
 		{#each ringRadii as radius}
 			<text
@@ -55,10 +59,18 @@
 				{radius}
 			</text>
 			<circle
+				filter="url(#blur)"
 				mask="url(#ringMask)"
 				r={(100 / maxSpeed) * radius}
 				fill="none"
 				stroke={`hsl(${hueForSpeed(radius)}, 100%, 50%)`}
+			/>
+			<circle
+				mask="url(#ringMask)"
+				r={(100 / maxSpeed) * radius}
+				fill="none"
+				stroke={`hsl(${hueForSpeed(radius) - 30}, 100%, 50%)`}
+				stroke-width="0.25"
 			/>
 		{/each}
 	</g>
@@ -76,10 +88,19 @@
 	</g>
 	<path
 		id="pointer"
+		filter="url(#blur)"
 		d="M0,100 L-22.45139882897927,30.901699437494752 L-2.0244413695060732,2.786404500042062 A3.4441853748633044,3.4441853748633044 0 0 1 -2.024441369506074,-2.786404500042062 L-3.2756149440922124,-4.508497187473714 L0,-100 L3.275614944092212,-4.508497187473714 L2.0244413695060737,-2.786404500042062 A3.4441853748633044,3.4441853748633044 0 0 1 2.0244413695060737,2.786404500042062 L22.451398828979276,30.901699437494752 Z"
 		stroke="hsl(30, 100%, 50%)"
 		fill="none"
 		stroke-width="1"
+		style={`transform: rotate(${$rapid_wind[0].direction}deg)`}
+	/>
+	<path
+		id="pointer"
+		d="M0,100 L-22.45139882897927,30.901699437494752 L-2.0244413695060732,2.786404500042062 A3.4441853748633044,3.4441853748633044 0 0 1 -2.024441369506074,-2.786404500042062 L-3.2756149440922124,-4.508497187473714 L0,-100 L3.275614944092212,-4.508497187473714 L2.0244413695060737,-2.786404500042062 A3.4441853748633044,3.4441853748633044 0 0 1 2.0244413695060737,2.786404500042062 L22.451398828979276,30.901699437494752 Z"
+		stroke="hsl(45, 100%, 50%)"
+		fill="none"
+		stroke-width="0.25"
 		style={`transform: rotate(${$rapid_wind[0].direction}deg)`}
 	/>
 	<path
